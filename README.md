b9b72cebe8717f8bfd5e38f4450007c588e912f5
dmosc <osdavrm@gmail.com>
2022-03-25 22:05:58 -0600

    Add additional description and names into README.md

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # WebGLMusicVisualizer
-Proyecto para clase de Gráficas Computacionales
+Proyecto para clase de Gráficas Computacionales.
+
+* Jorge Alexander Giovannetti Pulido, A01283034
+* Daniel Alejandro David Sanchez, A00824566
+* Oscar Rodríguez, A01229551
 
 ## Descripción de proyecto
-El objetivo de este proyecto es desarrollar un visualizador de música, generando esta visualización proceduralmente con WebGL.
+El objetivo de este proyecto es desarrollar un visualizador de música con formato de ambientación generada
+con formas reactivas trimiensionales en WebGL.
--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const canvas = document.querySelector('#visualizer');
 const gl = canvas.getContext('webgl');
 
 if (!gl) {
-    throw new Error('WebGL not supported');
+  throw new Error('WebGL not supported');
 }
 
-const render = () => { 
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+const render = () => {
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    // Clear the canvas
-    gl.clearColor(0.25, 0.8, 0.6, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  // Clear the canvas
+  gl.clearColor(0.25, 0.8, 0.6, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
 
 render();
