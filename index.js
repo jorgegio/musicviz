af3cc60c3e43d94e8376dca51ce6191a8e331790
JorgeGiovannetti <giovannetti2000@gmail.com>
2022-05-02 01:26:31 -0500

    Migration to three js

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-const canvas = document.querySelector('#visualizer');
-const gl = canvas.getContext('webgl');
+import './index.css';
 
-if (!gl) {
-  throw new Error('WebGL not supported');
-}
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const render = (audioBuffer) => {
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  if (audioBuffer) {
-    // Clear screen with rhythm.
-    gl.clearColor(audioBuffer[0] / 1000, audioBuffer[1] / 1000, audioBuffer[2] / 1000, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#visualizer')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(5);
+
+const monkeyScale = 1.5;
+const monkeyMaterial = new THREE.MeshToonMaterial({ color: 0x30f0c0 });
+
+
+const loader = new GLTFLoader();
+loader.load(
+  'models/monkey.glb',
+  function (gltf) {
+    gltf.scene.children[ 0 ].scale.set(monkeyScale, monkeyScale, monkeyScale);
+    gltf.scene.children[ 0 ].material = monkeyMaterial;
+    const root = gltf.scene;
+    scene.add(root);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+  },
+  (error) => {
+    console.log(error);
   }
+);
+
+scene.background = new THREE.Color(0xffffff);
+const light = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  render();
 }
 
-render();
-window.addEventListener('resize', render);
+function render() {
+  renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
+
+animate();
